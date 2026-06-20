b822624cc0532f0c5af9c3dc9feda03a7087ec3a
Al-Amin Reza <alaminreza6379@gmail.com>
2026-06-20 18:14:51 +0900

    Module built..

---
+++ src/main/java/com/torloksz/arethium/ArethiumApplication.java
@@ -0,0 +1,13 @@
+package com.torloksz.arethium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArethiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArethiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/torloksz/arethium/ArethiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torloksz.arethium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArethiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
